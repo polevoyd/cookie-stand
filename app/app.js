43668e9f9f1078041bb728baa7c3edda21c8f4e8
Dmitry <manxxeta@gmail.com>
2018-07-16 15:48:47 -0700

    building objects

--- app/app.js
+++ app/app.js
@@ -3,7 +3,11 @@
 
 
 var oclock = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total:'];
+//var storesArr = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+
+
 var unorderedListElements = document.createElement('ul');
+
 
 for(var i = 0; i < oclock.length; i++) {
   var listElement = document.createElement('li');
