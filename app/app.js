176a75f6ba49219affd2bb3f23c4f471a757a47c
Dmitry <manxxeta@gmail.com>
2018-07-16 15:55:33 -0700

    building objects

--- app/app.js
+++ app/app.js
@@ -1,12 +1,34 @@
 'use strict';
 
-
-
 var oclock = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total:'];
+
+// store objects
+var storePike = 
+{
+    minCustomersPerLocation: 23,
+    maxCustomersPerLocation: 65,
+    avgCookiePerSale: 6.3,
+
+    generateRndCustomersPerHour: function (min, max) 
+    {
+        return Math.random() * (max - min) + min;
+    },
+
+    for (var i = 0; i < oclock.length; i++)
+    {
+
+    },
+    
+
+
+};
+
+
 //var storesArr = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
 
 
 var unorderedListElements = document.createElement('ul');
+
 
 
 for(var i = 0; i < oclock.length; i++) {
