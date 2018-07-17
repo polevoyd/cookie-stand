2f1e5963633afba3878c66443faef6b5360dada7
Dmitry <manxxeta@gmail.com>
2018-07-17 14:04:51 -0700

    One cell working

--- app/app.js
+++ app/app.js
@@ -1,38 +1,93 @@
 'use strict';
 
 
-var oclock = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var storesArr = [];
 /*******************************************************************************************/
 /*********************************CREATING A CLASS******************************************/
 /*******************************************************************************************/
 
 function Store(name, minCus, maxCus, avgCookieSale)
 {
-    this.name = name;
-    this.minCus = minCus;
-    this.maxCus = maxCus;
-    this.avgCookieSale = avgCookieSale;
-    this.total = 0;
+  this.name = name;
+  this.minCus = minCus;
+  this.maxCus = maxCus;
+  this.avgCookieSale = avgCookieSale;
+  this.total = 0;
+  this.hoursSales = [];
 
-    storesArr.push(this);
+  storesArr.push(this);
+
+  Store.prototype.generateRndCustomersPerHour = function(min, max)
+  {
+    return Math.random() * (max - min) + min;
+  };
+
+  Store.prototype.generateAvgCookieSale = function()
+  {
+    return Math.floor(this.generateRndCustomersPerHour(this.minCus, this.maxCus) * this.avgCookieSale);
+  };
 
 }
 
-Store.prototype.generateRndCustomersPerHour = function(min, max)
-{
-    return Math.random() * (max - min) + min;
-};
+/*******************************************************************************************/
+/*******************************************************************************************/
+/*******************************************************************************************/
 
 
-
-
-
-
-
-
-
+// instance of store
 new Store('1st and Pike', 23, 65, 6.3);
+
+
+
+
+
+// creating a table
+var tableElement = document.getElementById('storesData');
+
+// for each store we create a row
+for (var st of storesArr)
+{
+// create a row 
+  var trElement = document.createElement('tr'); 
+
+  // create a first cell and put a store name there
+  // create a cell
+  var tdElement = document.createElement('td');
+
+
+}
+
+
+  
+// create a cell
+var tdElement = document.createElement('td');
+// create a data for a cell
+var textElement = document.createTextNode(storesArr[0].generateAvgCookieSale());
+// connect text to a cell
+tdElement.appendChild(textElement);
+// connect cell to a row
+trElement.appendChild(tdElement);
+// connect row to a table
+tableElement.appendChild(trElement);
+// connect table to a page
+document.body.appendChild(tableElement);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
