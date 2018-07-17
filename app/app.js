d4a6cdf78c89afed24cec0caac5c9fd41a341644
Dmitry <manxxeta@gmail.com>
2018-07-16 17:38:20 -0700

    final

--- app/app.js
+++ app/app.js
@@ -6,9 +6,11 @@ var oclock = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', 
 // store objects
 var storePike = 
 {
+  storeName: '1st and Pike',
   minCustomersPerLocation: 23,
   maxCustomersPerLocation: 65,
   avgCookiePerSale: 6.3,
+  total: 0,
 
   generateRndCustomersPerHour: function (min, max) 
   {
@@ -17,31 +19,31 @@ var storePike =
 
   generateString: function ()
   {
+    var monthsSection = document.getElementById('stores');
+
+    // name of the store 
+    var storeNameElement = document.createElement('h3');
+    storeNameElement.textContent = this.storeName;
+    monthsSection.appendChild(storeNameElement);
+    
+    // listElement.textContent = this.storeName;
     for (var i = 0; i < oclock.length; i++)
     {
+      // creating a number and a string to pring 
       var num = this.generateRndCustomersPerHour(this.minCustomersPerLocation, this.maxCustomersPerLocation) * this.avgCookiePerSale;
-
       var s = oclock[i] + ': ' + Math.floor(num) + ' cookies';
+      this.total += num;
 
+      // creating unordered list to put values in 
       var unorderedListElements = document.createElement('ul');
-
       var listElement = document.createElement('li');
       listElement.textContent = s;
       unorderedListElements.appendChild(listElement);
-    
-      var monthsSection = document.getElementById('stores');
       monthsSection.appendChild(unorderedListElements);
-
-
-      
     }
+
+    listElement.textContent = 'Total: ' + Math.floor(this.total) + ' cookies';
   }
-
-    
-  
-
-
 };
 storePike.generateString();
 
-//var storesArr = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
