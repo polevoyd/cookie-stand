fc65b1dd049e0fb269f613b879c9db86a421fabe
Dmitry <manxxeta@gmail.com>
2018-07-17 11:35:19 -0700

    Add testing files

---
+++ test_app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+function Dog(name, color, breed, nickname)
+{
+  this.name = name;
+  this.color = color;
+  this.breed = breed;
+  this.nickname = nickname;
+}
+
+
+var demi = new Dog('Demi', 'black', 'colie', 'dem-dem');
+
+// 'new' keyword does a few things
+//1. creates empty obj
+//2. assing values
+
+
+
+// create a document
